d0329c94eb029f4cabe83a9173dec803166cbfd0
nocarni <noahhcarniglia@gmail.com>
2023-09-15 12:27:49 -0700

    working on form validation server side

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"noahcarniglia/bookings/internal/config"
+	"noahcarniglia/bookings/internal/forms"
 	"noahcarniglia/bookings/internal/models"
 	"noahcarniglia/bookings/internal/render"
 )
@@ -49,7 +50,40 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 // Generals renders the generals-quarters page
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r, "make-reservation.html", &models.TemplateData{})
+	render.RenderTemplate(w, r, "make-reservation.html", &models.TemplateData{
+		Form: forms.New(nil),
+	})
+}
+
+// PostReservation handles the posting of a reservation form
+func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	reservation := models.Reservation{
+		FirstName: r.Form.Get("first_name"),
+		LastName:  r.Form.Get("last_name"),
+		Email:     r.Form.Get("email"),
+		Phone:     r.Form.Get("phone"),
+	}
+
+	form := forms.New(r.PostForm)
+
+	form.Has("first_name", r)
+
+	if !form.Valid() {
+		data := make(map[string]interface{})
+		data["reservation"] = reservation
+
+		render.RenderTemplate(w, r, "make-reservation.html", &models.TemplateData{
+			Form: form,
+			Data: data,
+		})
+		return
+	}
 }
 
 // Generals renders the generals-quarters page
